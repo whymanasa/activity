bfbe799f30e52ae3a1060512d811aeae9ea998f3
manasa y <123yapitsme@gmail.com>
2024-03-30 12:05:05 +0530

    adding things into app.js
    project1

--- color-flipper/app.js
+++ color-flipper/app.js
@@ -3,7 +3,11 @@ const btn= document.getElementById('btn');
 const color= document.querySelector('.color');
 
 btn.addEventListener('click',function(){
-    randomNumber=2;
+    const randomNumber=getRandomNumber();
     document.body.style.backgroundColor= colors[randomNumber];
     color.textContent=colors[randomNumber];
-})
+});
+
+function getRandomNumber(){
+    return Math.floor(Math.random()*colors.length);
+}
