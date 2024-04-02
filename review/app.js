66d74be4c5917d4536b1f976f99716dbb916a815
manasa y <123yapitsme@gmail.com>
2024-04-02 21:52:49 +0530

    completed the js
    project 3

--- review/app.js
+++ review/app.js
@@ -35,3 +35,37 @@ const info =document.getElementById('info');
 const prevBtn=document.querySelector('.prev-btn');
 const nextBtn=document.querySelector('.next-btn');
 const randomBtn=document.querySelector('.random-btn');
+
+function showPerson(person) {
+    const item= reviews[person];
+    img.src=item.img;
+    author.textContent=item.name;
+    info.textContent=item.text;
+    job.textContent=item.job; 
+};
+
+let currentItem=0;
+window.addEventListener('DOMContentLoaded',function () {
+    showPerson(currentItem);
+});
+
+nextBtn.addEventListener('click',function(){
+    currentItem++;
+    if (currentItem>reviews.length-1) {
+        currentItem=0;
+    }
+    showPerson(currentItem);
+});
+
+prevBtn.addEventListener('click',function(){
+    currentItem--;
+    if (currentItem<0) {
+        currentItem=reviews.length-1;
+    }
+    showPerson(currentItem);
+})
+
+randomBtn.addEventListener('click',function(){
+    currentItem=Math.floor(Math.random()*(reviews.length));
+    showPerson(currentItem);
+});
